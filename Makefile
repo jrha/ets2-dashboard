b310f2c28d17241f1751154200cfc555d9a11016
James Adams <james@bluezen.co.uk>
2018-07-31 00:22:22 +0100

    Quick and dirty first pass plugin
    Based on examples and copy-and-pasting from stack overflow.
    
    But it works!

---
+++ Makefile
@@ -0,0 +1,28 @@
+SDK_HEADERS=\
+	./*.h \
+	../../include/*.h \
+	../../include/common/*.h \
+	../../include/amtrucks/*.h \
+	../../include/eurotrucks2/*.h
+
+SDK_INCLUDES=\
+	-I. \
+	-I../../include \
+	-I../../include/common/ \
+	-I../../include/amtrucks/ \
+	-I../../include/eurotrucks2
+
+UNAME:= $(shell uname -s)
+
+ifeq ($(UNAME),Darwin)
+LIB_NAME_OPTION=-install_name
+else
+LIB_NAME_OPTION=-soname
+endif
+
+telemetry.so:  *.cpp $(SDK_HEADERS)
+	gcc -o $@ -fPIC -Wall --shared -Wl,$(LIB_NAME_OPTION),$@ $(SDK_INCLUDES) -std=c++11 *.cpp
+
+.PHONY: clean
+clean:
+	@rm -f -- *.so
---
+++ serial.h
@@ -0,0 +1,113 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <termios.h>
+#include <unistd.h>
+
+int set_interface_attribs(int fd, int speed)
+{
+    struct termios tty;
+
+    if (tcgetattr(fd, &tty) < 0) {
+        printf("Error from tcgetattr: %s\n", strerror(errno));
+        return -1;
+    }
+
+    cfsetospeed(&tty, (speed_t)speed);
+    cfsetispeed(&tty, (speed_t)speed);
+
+    tty.c_cflag |= (CLOCAL | CREAD);    /* ignore modem controls */
+    tty.c_cflag &= ~CSIZE;
+    tty.c_cflag |= CS8;         /* 8-bit characters */
+    tty.c_cflag &= ~PARENB;     /* no parity bit */
+    tty.c_cflag &= ~CSTOPB;     /* only need 1 stop bit */
+    tty.c_cflag &= ~CRTSCTS;    /* no hardware flowcontrol */
+
+    /* setup for non-canonical mode */
+    tty.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
+    tty.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
+    tty.c_oflag &= ~OPOST;
+
+    /* fetch bytes as they become available */
+    tty.c_cc[VMIN] = 1;
+    tty.c_cc[VTIME] = 1;
+
+    if (tcsetattr(fd, TCSANOW, &tty) != 0) {
+        printf("Error from tcsetattr: %s\n", strerror(errno));
+        return -1;
+    }
+    return 0;
+}
+
+void set_mincount(int fd, int mcount)
+{
+    struct termios tty;
+
+    if (tcgetattr(fd, &tty) < 0) {
+        printf("Error tcgetattr: %s\n", strerror(errno));
+        return;
+    }
+
+    tty.c_cc[VMIN] = mcount ? 1 : 0;
+    tty.c_cc[VTIME] = 5;        /* half second timer */
+
+    if (tcsetattr(fd, TCSANOW, &tty) < 0)
+        printf("Error tcsetattr: %s\n", strerror(errno));
+}
+
+
+int serial_connect(const char *portname) {
+    int fd;
+
+    FILE *log_file = NULL;
+
+    log_file = fopen("serial.log", "wt");
+
+    fd = open(portname, O_RDWR | O_NOCTTY | O_SYNC);
+    if (fd < 0) {
+        fprintf(log_file, "Error opening %s: %s\n", portname, strerror(errno));
+        return -1;
+    }
+    /*baudrate 115200, 8 bits, no parity, 1 stop bit */
+    set_interface_attribs(fd, B115200);
+    //set_mincount(fd, 0);                /* set to pure timed read */
+
+    return fd;
+}
+
+void serial_write(int fd) {
+    int wlen;
+    FILE *log_file = NULL;
+    log_file = fopen("serial.log", "wt");
+    wlen = write(fd, "Hello!\n", 7);
+    if (wlen != 7) {
+        fprintf(log_file, "Error from write: %d, %d\n", wlen, errno);
+    }
+}
+
+void serial_read(int fd) {
+    /* simple noncanonical input */
+    do {
+        unsigned char buf[80];
+        int rdlen;
+
+        rdlen = read(fd, buf, sizeof(buf) - 1);
+        if (rdlen > 0) {
+#ifdef DISPLAY_STRING
+            buf[rdlen] = 0;
+            printf("Read %d: \"%s\"\n", rdlen, buf);
+#else /* display hex */
+            unsigned char   *p;
+            printf("Read %d:", rdlen);
+            for (p = buf; rdlen-- > 0; p++)
+                printf(" 0x%x", *p);
+            printf("\n");
+#endif
+        } else if (rdlen < 0) {
+            printf("Error from read: %d: %s\n", rdlen, strerror(errno));
+        }
+        /* repeat read to get full message */
+    } while (1);
+}
