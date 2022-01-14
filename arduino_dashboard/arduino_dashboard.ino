0a414eca388c1a60f5cbfe0791153e1cdde6cecf
James Adams <james@bluezen.co.uk>
2022-01-14 11:28:31 +0000

    Replace 7-segment display with OLED
    Display layout based on TGX computer in game.

--- arduino_dashboard/arduino_dashboard.ino
+++ arduino_dashboard/arduino_dashboard.ino
@@ -1,6 +1,13 @@
 #include <Servo.h>
 #include <TM1637Display.h>
 
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Fonts/Picopixel.h>
+
+#include "dashb-lcd.h"
 
 String property = "";
 String value = "";
@@ -11,14 +18,10 @@ boolean stringComplete = false;
 Servo servo_speed;
 Servo servo_rpm;
 
-TM1637Display display(2, 3); //clk, dio
-static uint8_t BLANK[] = { 0x0, 0x0, 0x0, 0x0 };
-static uint8_t INIT[] = { 0x0, SEG_G, SEG_G, 0x0};
-//                             .GFEDCBA
-static uint8_t NEUTRAL[] = { 0b00110111, 0x0, 0x0, 0x0 };
-static uint8_t FORWARD[] = { 0b01110001, 0x0, 0x0, 0x0 };
-static uint8_t REVERSE[] = { 0b00110011, 0x0, 0x0, 0x0 };
-static uint8_t T_COLON[] = { 0x0, 0b10000000, 0x0, 0x0 };
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 
 void serialEvent() {
@@ -56,9 +59,22 @@ void setup() {
   servo_speed.write(180);
   servo_rpm.write(180);
 
-  display.setSegments(BLANK);
-  display.setBrightness(0x1);
-  display.setSegments(INIT);
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!oled.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  oled.clearDisplay();
+
+  oled.drawBitmap(0, 0, background_data, SCREEN_WIDTH, SCREEN_HEIGHT, 1);
+  oled.display();
+
+  oled.setCursor(7, 10);
+  oled.setTextSize(1);
+  oled.setTextColor(WHITE);
+  oled.print("_");
+  oled.display();
 
   Serial.begin(115200);
   while (!Serial) {
@@ -88,6 +104,14 @@ void loop() {
 
     if (property == "truck.speed") {
       servo_speed.write(180 - value.toInt());
+
+      oled.fillRect(75, 36, 50, 10, SSD1306_BLACK);
+      oled.setTextSize(1);
+      oled.setCursor(5, 36);
+      oled.print("SPEED");
+      oled.setCursor(75, 36);
+      oled.print(value);
+      oled.display();
     } else if (property == "truck.engine.rpm") {
       servo_rpm.write(180 - value.toInt());
     } else if (property == "truck.light.rblinker") {
@@ -103,18 +127,52 @@ void loop() {
     } else if (property == "truck.displayed.gear") {
       int v = value.toInt();
       if (v > 0) {
-        display.setSegments(FORWARD);
-        display.showNumberDec(v, false, 2, 1);
+        oled.fillRect(6, 8, 7, 10, SSD1306_BLACK);
+        oled.fillRect(17, 6, 24, 16, SSD1306_BLACK);
+
+        oled.setCursor(7, 10);
+        oled.setTextSize(1);
+        oled.print("A");
+
+        oled.setCursor(17, 6);
+        oled.setTextSize(2);
+        oled.print(v);
+
+        oled.display();
       } else if (v == 0) {
-        display.setSegments(NEUTRAL);
-        display.showNumberDec(v, false, 2, 1);
+        oled.fillRect(6, 8, 7, 10, SSD1306_BLACK);
+        oled.fillRect(17, 6, 24, 16, SSD1306_BLACK);
+
+        oled.setCursor(7, 10);
+        oled.setTextSize(1);
+        oled.print("N");
+
+        oled.display();
       } else {
-        display.setSegments(REVERSE);
-        display.showNumberDec(v*-1, false, 2, 1);
+        oled.fillRect(6, 8, 7, 10, SSD1306_BLACK);
+        oled.fillRect(17, 6, 24, 16, SSD1306_BLACK);
+
+        oled.setCursor(7, 10);
+        oled.setTextSize(1);
+        oled.print("R");
+
+        oled.setCursor(17, 6);
+        oled.setTextSize(2);
+        oled.print(v*-1);
+
+        oled.display();
       }
     } else if (property == "game.time") {
-      int v = value.toInt();
-      display.showNumberDecEx(v, 0b01000000, true, 4, 0);
+      String t = String(value[0]);
+      t.concat(value[1]);
+      t.concat(":");
+      t.concat(value[2]);
+      t.concat(value[3]);
+      oled.fillRect(3, 54, 30, 8, SSD1306_BLACK);
+      oled.setTextSize(1);
+      oled.setCursor(3, 54);
+      oled.print(t);
+      oled.display();
     } else {
       ; //Serial.println("NOPE");
     }
